1fb2d58e97766e5d31b653add99765292f1879e5
I like turtles <blue's commit>
2014-07-03 14:19:30 -0400

    started working on wall strategy

---
+++ RobotWar.spritebuilder/Source/GeniusRobot.h
@@ -0,0 +1,13 @@
+//
+//  GeniusRobot.h
+//  RobotWar
+//
+//  Created by Melanie H. on 7/3/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface GeniusRobot : CCNode
+
+@end
